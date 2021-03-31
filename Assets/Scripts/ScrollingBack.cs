25c0d10c5b354a757654d48cbb961b96d59db704
Bsanda21 <46551065+Bsanda21@users.noreply.github.com>
2021-03-30 22:26:26 -0400

    Minor
    Minor changes

--- Assets/Scripts/ScrollingBack.cs
+++ Assets/Scripts/ScrollingBack.cs
@@ -9,7 +9,8 @@ public class ScrollingBack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Bspeed = 0.4;
+
     }
 
     // Update is called once per frame
