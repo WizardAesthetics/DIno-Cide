50759acf152d401f6a38c788819eef2fc9891e90
StickyKeysEnabled <acrawf26@emich.edu>
2021-04-18 17:48:17 -0400

    Add volume setting

---
+++ Assets/Scripts/VolumeChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeChange : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
